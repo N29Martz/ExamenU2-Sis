bde237f456ead97244c18f29edb8262fd111b3a5
Angel1111111111111 <angelnataren16@gmail.com>
2024-07-26 15:17:53 -0600

    Configurations

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Component</Text>
+    </NavigationContainer>
+  );
+};
